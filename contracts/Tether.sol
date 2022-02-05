1058a3aebdfa4dbd9bffe8003bf717ca0e568815
graphica99 <mendsalbert@gmail.com>
2022-02-05 15:51:52 +0000

    Tether contract

--- contracts/Tether.sol
+++ contracts/Tether.sol
@@ -8,6 +8,22 @@ contract Tether {
 
     event Transfer(address indexed _from, address indexed _to , uint _value);
     event Approve( address indexed _owner , address indexed _spender, uint _value);
-    
+
+    mapping(adddress => uint256) public balaceOf;
+
+    constructor() {
+        balaceOf[msg.sender] = totalSupply
+    }
+
+    function transfer(address _to , uint _value) public returns (bool success){
+        require(balanceOf[msg.sender] >= _value);
+        balaceOf[msg.sender] -= _value;
+        balaceOf[_to]+=_value
+        emit Transfer(msg.sender, _to, _value);
+        return true
+    }
+
+
+
 
 }
