e3e12a9de9b783d1924364cf4d7e41c80f9296d7
graphica99 <mendsalbert@gmail.com>
2022-02-05 15:45:54 +0000

    Tether contract

--- contracts/Tether.sol
+++ contracts/Tether.sol
@@ -5,4 +5,9 @@ contract Tether {
     string public symbol = 'USDT';
     uint256 public totalSupply = 1000000000000000000000000;
     uint8 public decimals = 18;
+
+    event Transfer(address indexed _from, address indexed _to , uint _value);
+    event Approve( address indexed _owner , address indexed _spender, uint _value);
+    
+
 }
