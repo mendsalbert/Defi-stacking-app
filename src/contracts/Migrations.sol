58236cfcb76c6f4a805878ac680690cc39ae3868
graphica99 <mendsalbert@gmail.com>
2022-02-05 14:39:14 +0000

    migragtion smart contract

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -1,4 +1,4 @@
-pragma solidity '^0.5.0';
+pragma solidity ^0.5.0;
 
 contract Migrations{
   address public owner;
@@ -12,13 +12,21 @@ contract Migrations{
       if(msg.sender == owner) _;
   }
 
-  function set_completed(uint completed) public restricted{
+  function setCompleted(uint completed) public restricted{
        last_completed_migration = completed;
   }
 
+//when the block chain is running we used this upgrade function to 
+//upgrade our smart contract
+
+/**
+ so what this code is basically doing here is to make 
+ create a new instance of the a current address (owner address)
+ set it last completedd to current completed state
+ */
   function upgrade(address new_address) public restricted{
     Migrations upgraded = Migrations(new_address);
-    upgraded.set_completed(last_completed_migration);
+    upgraded.setCompleted(last_completed_migration);
   }
 
 }
