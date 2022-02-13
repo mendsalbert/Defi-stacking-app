1cc5d54c30bfd6e697354d7496414b557b1b8fae
graphica99 <mendsalbert@gmail.com>
2022-02-13 18:22:37 +0000

    testing

--- test/decentralBank.test.js
+++ test/decentralBank.test.js
@@ -1,5 +1,3 @@
-const { default: Web3 } = require("web3");
-
 const DecentralBank = artifacts.require("DecentralBank");
 const Reward = artifacts.require("RWD");
 const Tether = artifacts.require("Tether");
@@ -8,17 +6,18 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("DecentralBank", (accounts) => {
+contract("DecentralBank", ([owner, customer]) => {
   let tether, reward, decentralBank;
 
   function tokens(number) {
-    return Web3.utils.toWei(number);
+    return web3.utils.toWei(number, "ether");
   }
   before(async () => {
     tether = await Tether.new();
     reward = await Reward.new();
     decentralBank = await DecentralBank.new(reward.address, tether.address);
     await reward.transfer(decentralBank.address, tokens("1000000"));
+    await tether.transfer(customer, tokens("100"), { from: owner });
   });
   describe("Tether Deployement", async () => {
     it("matches name successfully", async () => {
@@ -33,8 +32,16 @@ contract("DecentralBank", (accounts) => {
       assert.equal(name, "Reward");
     });
   });
+
+  describe("DecentralBank Deployement", async () => {
+    it("matches name successfully", async () => {
+      const name = await decentralBank.name();
+      assert.equal(name, "Decentral Bank");
+    });
+
+    it("contract has tokens", async () => {
+      const balance = await reward.balanceOf(decentralBank.address);
+      assert.equal(balance, tokens("1000000"));
+    });
+  });
 });
-
-// contract("DecentralBank", (accounts) => {
-
-// });
