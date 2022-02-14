c3283875f95ac7ddd583b942bb8fa20f8c352c37
graphica99 <mendsalbert@gmail.com>
2022-02-14 06:07:50 +0000

    added frontend ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import bank from "../bankk.png";
 const Navbar = () => {
   return (
-    <nav className="bg-black py-3 text-white">
+    <nav className="bg-black py-2 text-white">
       <div className="flex flex-row justify-between p-5">
-        <p className="text-2xl">DDAP Yield Staking (Decentralised Bank)</p>
+        <div className="flex items-center ">
+          <p className="text-2xl">DDAP Yield Staking (Decentralised Bank)</p>
+          <img src={bank} width={85} />
+        </div>
         <p className="text-lg">Account Number</p>
       </div>
     </nav>
