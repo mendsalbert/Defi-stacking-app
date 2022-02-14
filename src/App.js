1399b21a195c951661663c2ce450d5049121c71c
graphica99 <mendsalbert@gmail.com>
2022-02-14 06:27:48 +0000

    added frontend ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 // import Button from "react-bootstrap/Button";
+import web3 from "web3";
+import Web3 from "web3";
+useEffect(() => {
+  loadWeb3();
+}, []);
+
+const loadWeb3 = async () => {
+  if (window.ethereuem) {
+    window.web3 = new Web3(window.ethereuem);
+  } else if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider);
+  } else {
+    window.alert("no ethereum browser detected");
+  }
+};
 
 const App = () => {
+  const [accountNumber, setAccoutNumber] = useState("0x0");
   return (
     <div>
-      <Navbar />
+      <Navbar accountNumber={accountNumber} />
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import bank from "../bankk.png";
-const Navbar = () => {
+const Navbar = ({ accountNumber }) => {
   return (
-    <nav className="bg-black py-2 text-white">
-      <div className="flex flex-row justify-between p-5">
+    <nav className="bg-black text-white">
+      <div className="flex flex-row items-center py-3 justify-between ">
         <div className="flex items-center ">
-          <p className="text-2xl">DDAP Yield Staking (Decentralised Bank)</p>
+          <p className="text-lg">DDAP Yield Staking (Decentralised Bank)</p>
           <img src={bank} width={85} />
         </div>
-        <p className="text-lg">Account Number</p>
+        <p className="text-lg">ACCOUNT NUMBER : {accountNumber}</p>
       </div>
     </nav>
   );
