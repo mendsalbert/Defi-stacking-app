d8a2dd419817844efda841d54fa82d5d547e3726
graphica99 <mendsalbert@gmail.com>
2022-02-14 05:46:45 +0000

    added frontend ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 // import Button from "react-bootstrap/Button";
 
 const App = () => {
   return (
     <div className="text-center">
+      <Navbar />
       <p>hello world</p>
       {/* <Button>button</Button> */}
     </div>
