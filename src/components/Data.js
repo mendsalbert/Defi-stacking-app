62fc1331d7757b93a0e97fab9b636792045c89eb
graphica99 <mendsalbert@gmail.com>
2022-02-16 16:35:37 +0000

    frontend ui

--- src/components/Data.js
+++ src/components/Data.js
@@ -11,18 +11,28 @@ import {
 const Data = () => {
   return (
     // <div className=" mt-12 w-8/12  rounded-lg text-gray-600 px-16 p-4  flex justify-between items-center">
-    <div className="flex flex-row justify-between mt-8 space-x-4  w-8/12">
-      <div className="w-full rounded-md text-white py-8 bg-black flex flex-col items-center justify-center ">
-        <LightningBoltIcon className="h-14" />
-        <p className="text-2xl">Staking Tokens</p>
-        <p className="text-7xl font-bold">0</p>
+    <>
+      <div className="flex flex-row justify-between mt-8 space-x-4  w-8/12">
+        <div className="w-full rounded-md text-white py-8 bg-black flex flex-col items-center justify-center ">
+          <LightningBoltIcon className="h-14" />
+          <p className="text-2xl">Staking Tokens</p>
+          <p className="text-7xl font-bold">0</p>
+        </div>
+        <div className="w-full rounded-md text-white bg-black flex flex-col items-center justify-center ">
+          <CubeIcon className="h-14" />
+          <p className="text-2xl">Balance</p>
+          <p className="text-7xl font-bold">100</p>
+        </div>
       </div>
-      <div className="w-full rounded-md text-white bg-black flex flex-col items-center justify-center ">
-        <CubeIcon className="h-14" />
-        <p className="text-2xl">Balance</p>
-        <p className="text-7xl font-bold">100</p>
+      <div className=" space-y-4 flex w-8/12 text-center flex-col items-center justify-center mt-4">
+        <div className="w-full text-white text-xl cursor-pointer bg-green-700 py-3 rounded-md">
+          DEPOSITE
+        </div>
+        <div className="w-full text-white text-xl cursor-pointer bg-red-600 py-3 rounded-md">
+          WITHDRAW
+        </div>
       </div>
-    </div>
+    </>
     // </div>
   );
 };
