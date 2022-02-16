afe768f8ee3d819f21d1b53842f3287ae07b2b1d
graphica99 <mendsalbert@gmail.com>
2022-02-16 04:21:26 +0000

    updated webpack page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
-// import Button from "react-bootstrap/Button";
-import web3 from "web3";
 import Web3 from "web3";
-useEffect(() => {
-  loadWeb3();
-}, []);
-
-const loadWeb3 = async () => {
-  if (window.ethereuem) {
-    window.web3 = new Web3(window.ethereuem);
-  } else if (window.web3) {
-    window.web3 = new Web3(window.web3.currentProvider);
-  } else {
-    window.alert("no ethereum browser detected");
-  }
-};
 
 const App = () => {
+  const loadWeb3 = async () => {
+    if (window.ethereuem) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereuem.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("no ethereum browser detected");
+    }
+  };
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+    const account = await web3.eth.getAccounts();
+    console.log(account);
+  };
+  useEffect(() => {
+    loadWeb3();
+    loadBlockchainData();
+  }, []);
+
   const [accountNumber, setAccoutNumber] = useState("0x0");
   return (
     <div>
