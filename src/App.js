aab8333de3b8ec79d1e2aba430ecbbbe39e0571c
graphica99 <mendsalbert@gmail.com>
2022-02-16 15:43:00 +0000

    frontend ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Web3 from "web3";
+import "./assets/main.css";
+// import Tether from "../build/contracts/Tether.json";
+import Tether from "./build/Tether.json";
+import Reward from "./build/RWD.json";
+import DecentralBank from "./build/DecentralBank.json";
 
 const App = () => {
   const loadWeb3 = async () => {
@@ -18,8 +23,49 @@ const App = () => {
     const web3 = window.web3;
     const account = await web3.eth.getAccounts();
     setAccoutNumber(account);
-    console.log(account);
+
+    const networkId = await web3.eth.net.getId();
+    const tetherData = await Tether.networks[networkId];
+
+    if (tetherData) {
+      const tether = new web3.eth.Contract(Tether.abi, tetherData.address);
+      setTether({ tether });
+      let tetherBalance = await tether.methods.balanceOf(account[0]).call();
+      setTetherBalance({ tetherBalance: tetherBalance.toString() });
+      console.log(tetherBalance);
+    } else {
+      window.alert("tether contract not deployed");
+    }
+    const rewardData = await Reward.networks[networkId];
+    if (reward) {
+      const reward = new web3.eth.Contract(Reward.abi, rewardData.address);
+      setReward({ reward });
+      let rewardBalance = await reward.methods.balanceOf(account[0]).call();
+      setRewardBalance({ rewardBalance: rewardBalance.toString() });
+      console.log(rewardBalance);
+    } else {
+      window.alert("reward contract not deployed");
+    }
+
+    const decentralBankData = await DecentralBank.networks[networkId];
+    if (decentralBank) {
+      const decentralBank = new web3.eth.Contract(
+        DecentralBank.abi,
+        decentralBankData.address
+      );
+      setDecentralBank({ decentralBank });
+      let stakingBalance = await decentralBank.methods
+        .stakingBalance(account[0])
+        .call();
+      setStakingBalance(stakingBalance);
+      console.log(stakingBalance);
+    } else {
+      window.alert("decentral bank not deployed");
+    }
+
+    setLoading(false);
   };
+
   useEffect(() => {
     loadWeb3();
     loadBlockchainData();
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import bank from "../bankk.png";
 const Navbar = ({ accountNumber }) => {
   return (
     <nav className="bg-black text-white">
-      <div className="flex flex-row items-center py-3 justify-between ">
+      <div className="flex flex-row items-center py-3  justify-between ">
         <div className="flex items-center ">
           <p className="text-lg">DDAP Yield Staking (Decentralised Bank)</p>
           <img src={bank} width={85} />
